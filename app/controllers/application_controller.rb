ac3312a3927d9783af0091bb9a3a794635e3ddfd
RYOKOtakahashi <ryoko.1109@gmail.com>
2018-09-19 19:19:09 +0900

    sign up & login
    .
    
    i can login
    
    add:  gem 'devise'
    
    add: devise User M
    
    modify: migrate/_devise_create_users
    
    add: V for devise
    
    modify: V/devise/registrations/new
    
    modify: V/devise/sessions/new
    
    modify: V/devise/shared/_links
    
    modify: config/initializers/devise
    
    modify: C/application
    
    modify: M/user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+
+	before_action :configure_permitted_parameters, if: :devise_controller?
+  # deviseを利用する機能（ユーザ登録、ログイン認証など）が実行される前に、configure_permitted_parametersが実行される
+
+
+
+ protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name])
+  end
+  # configure_permitted_parametersでは、devise_parameter_sanitizer.permitでnameのデータ操作を許可するアクションメソッドが指定されている。
+  # 今回の場合、ユーザ登録(sign_up)の際、ユーザ名(name)のデータ操作が許可されることになる。
+  # ストロングパラメータと同様。
+  # privateは自分のコントローラ内でしか参照できないが、protectedは呼び出された他のコントローラからも参照ができるストロングパラメータとなる。
+
 end
